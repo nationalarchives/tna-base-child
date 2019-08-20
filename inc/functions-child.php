7c1242e9a41ab272649b3ac76c4c00ed0de80b4a
domingobishop <domingobishop@gmail.com>
2019-08-20 15:20:52 +0100

    rename css file

--- inc/functions-child.php
+++ inc/functions-child.php
@@ -12,7 +12,7 @@ function tna_child_styles()
 {
 	wp_register_style('tna-parent-styles', get_template_directory_uri() . '/css/base-sass.min.css', array(),
 		EDD_VERSION, 'all');
-	wp_register_style('tna-child-styles', get_stylesheet_directory_uri() . '/css/child-sass.css.min', array(), '0.1', 'all');
+	wp_register_style('tna-child-styles', get_stylesheet_directory_uri() . '/css/child-sass.min.css', array(), '0.1', 'all');
 	wp_enqueue_style('tna-parent-styles');
 	wp_enqueue_style('tna-child-styles');
 }
